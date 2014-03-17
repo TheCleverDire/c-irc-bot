715393695b6956a7a79a30f0502ac01834832384
Calvin B <calvin@openmailbox.org>
2014-03-17 12:13:09 -0300

    DDG plugin

---
+++ IrcBot.Search/Search.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Meebey.SmartIrc4net;
+using System.Xml;
+using System.Web;
+
+namespace IrcBot.Search
+{
+    /// <summary>
+    /// Searches DuckDuckGo for a zero-click answer.
+    /// </summary>
+    public class Search : IPlugin
+    {
+        string IPlugin.InvokeWithMessage(string source, string message, ref IrcClient client)
+        {
+            string toSend = null;
+            if (message.StartsWith(".ddg"))
+            {
+                XmlDocument xd = new XmlDocument();
+                xd.Load(GetDDGApiUrl(message.Split(new char[] { ' ' }, 2)[1]));
+                if (xd.SelectSingleNode("/DuckDuckGoResponse/Answer") != null)
+                {
+                    toSend = xd.SelectSingleNode("/DuckDuckGoResponse/Answer").InnerText;
+                }
+                if (!xd.SelectSingleNode("/DuckDuckGoResponse/Abstract").InnerText.IsNullOrWhitespace())
+                {
+                    toSend = String.Format("{0} - {1}", xd.SelectSingleNode("/DuckDuckGoResponse/Abstract").InnerText, xd.SelectSingleNode("/DuckDuckGoResponse/AbstractURL").InnerText);
+                }
+            }
+            return toSend;
+        }
+
+        string IPlugin.InvokeWithChannelUserChange(string channel, string user, string kicker, string message, ChannelUserChange type, ref IrcClient client)
+        {
+            return null; // Not implemented
+        }
+
+        string GetDDGApiUrl(string query)
+        {
+            return String.Format("https://api.duckduckgo.com/?q={0}&format=xml&t=IrcBot.Search&no_redirect=1&no_html=1&skip_disambig=1", HttpUtility.UrlEncode(query));
+        }
+    }
+}
